294e5011511657671bc2e5090177ea5d872dcf87
Kaan Engin <enginkaan6@gmail.com>
2021-08-15 15:48:09 +0300

    Vue 3 and Vuetify.js are integrated

--- backend/resources/js/app.js
+++ backend/resources/js/app.js
@@ -1,1 +1,9 @@
-require('./bootstrap');
+import {createApp} from "vue";
+import App from "./components/App.vue";
+// import { createVuetify } from "vuetify";
+import vuetify from "./plugins/vuetify";
+const app = createApp(App)
+
+app.use(vuetify)
+
+app.mount('#app')
---
+++ backend/resources/js/components/App.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <v-app>
+    <div class="fill-height">
+      <v-btn block>
+        Block Button
+      </v-btn>
+    </div>
+  </v-app>
+
+</template>
+
+<style>
+
+</style>
+
+<script>
+export default {
+
+}
+</script>
+
+
